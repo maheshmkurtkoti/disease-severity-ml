85e6d7530e4111f3f80f1f16adf3e84525a78589
Mahesh Kurtkoti <maheshmkurtkoti@gmail.com>
2026-02-17 15:12:14 -0500

    transforms and dataset flow design

---
+++ src/data/brain_ct_dataset.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from PIL import Image
+from torch.utils.data import Dataset
+
+from src.utils.transforms import get_train_transforms, get_val_transforms
+from src.utils.severity_mapper import map_label
+
+
+class BrainCTDataset(Dataset):
+    def __init__(self, config, split="train"):
+        self.root = Path(config["dataset_path"]) / split
+        self.image_paths = list(self.root.glob("*/*.png"))
+
+        self.transform = (
+            get_train_transforms(config["image_size"])
+            if split == "train"
+            else get_val_transforms(config["image_size"])
+        )
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+
+        image = Image.open(img_path).convert("RGB")
+        label_str = img_path.parent.name
+        label = map_label(label_str)
+
+        image = self.transform(image)
+
+        return image, label
---
+++ src/utils/transforms.py
@@ -0,0 +1,23 @@
+from torchvision import transforms
+
+def get_train_transforms(image_size=224):
+    return transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+    ])
+
+def get_val_transforms(image_size=224):
+    return transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        ),
+    ])
